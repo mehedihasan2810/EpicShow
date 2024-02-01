bc785015b200fcb7db1d91d1fe68c67c854d868f
mehedi-hasan <mdmehedihasan2810@gmail.com>
2024-02-01 14:34:48 +0600

    feat: add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,39 @@
-
+import { Link } from "react-router-dom";
+import "./footer.css";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div>
+        <Link to="/">Live shows</Link>
+        <Link to="/">Live News</Link>
+        <Link to="/">Live Sports</Link>
+        <Link to="/">Streaming Library</Link>
+      </div>
+      <div>
+        <Link to="/">TV shows</Link>
+        <Link to="/">Movies</Link>
+        <Link to="/">Originals</Link>
+        <Link to="/">Networks</Link>
+        <Link to="/">Kids</Link>
+        <Link to="/">Streaming Library</Link>
+        <Link to="/">Live Sports</Link>
+      </div>
+      <div>
+        <Link to="/">Max</Link>
+        <Link to="/">Cinemax</Link>
+        <Link to="/">STARZ</Link>
+        <Link to="/">Disney Premium</Link>
+        <Link to="/">Live shows</Link>
+      </div>
+      <div>
+        <Link to="/">Hulu Premium</Link>
+        <Link to="/">Premium Subscription</Link>
+        <Link to="/">About us</Link>
+        <Link to="/">Careers</Link>
+        <Link to="/">Terms $ conditions</Link>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
